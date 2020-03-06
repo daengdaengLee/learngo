f809db0520900a2ab4a0b10adac750360a8de67a
daengdaenglee <gunho1020@gmail.com>
2020-03-06 12:00:22 +0900

    변수와 상수 선언

--- main.go
+++ main.go
@@ -1,12 +1,17 @@
 package main
 
-import (
-	"fmt"
-
-	"github.com/daengdaengLee/learngo/something"
-)
+import "fmt"
 
 func main() {
-	fmt.Println("Hello World")
-	something.SayHello()
+	const name1 string = "kunho"
+	fmt.Println(name1)
+
+	var name2 string = "nico"
+	name2 = "lynn"
+	fmt.Println(name2)
+
+	// 함수 안에서만 가능한 축약 표현
+	name3 := "nico"
+	name3 = "lynn"
+	fmt.Println(name3)
 }
