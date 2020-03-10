69c4ee7f11cc917c0bcee438c8718fa9109fddae
daengdaenglee <gunho1020@gmail.com>
2020-03-10 11:47:13 +0900

    map

--- main.go
+++ main.go
@@ -3,12 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	names1 := [5]string{"noci", "lynn", "dal"}
-	names1[3] = "alala"
-	names1[4] = "alala"
-	fmt.Println(names1)
+	nico := map[string]string{
+		"name": "nico",
+		"age":  "12",
+	}
 
-	names2 := []string{"nico", "lynn", "dal"}
-	names2 = append(names2, "flynn")
-	fmt.Println(names2)
+	fmt.Println(nico)
 }
