5c099245f8d1e77ff5f6750e02d0810c0d040b00
daengdaenglee <gunho1020@gmail.com>
2020-03-10 11:03:52 +0900

    nacked return 사용법

--- main.go
+++ main.go
@@ -1,11 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
-func repeatMe(words ...string) {
-	fmt.Println(words)
+func lenAndUpper(name string) (length int, uppercase string) {
+	length = len(name)
+	uppercase = strings.ToUpper(name)
+	return
 }
 
 func main() {
-	repeatMe("nico", "lynn", "dal", "marl", "flynn")
+	totalLength, up := lenAndUpper("nico")
+	fmt.Println(totalLength, up)
 }
