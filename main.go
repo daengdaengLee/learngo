ff4cebe3640c288fe1bec5ece47ce5ff88fdbc2c
daengdaenglee <gunho1020@gmail.com>
2020-03-10 10:45:05 +0900

    기본적인 함수 작성법

--- main.go
+++ main.go
@@ -2,16 +2,10 @@ package main
 
 import "fmt"
 
+func multiply(a int, b int) int {
+	return a * b
+}
+
 func main() {
-	const name1 string = "kunho"
-	fmt.Println(name1)
-
-	var name2 string = "nico"
-	name2 = "lynn"
-	fmt.Println(name2)
-
-	// 함수 안에서만 가능한 축약 표현
-	name3 := "nico"
-	name3 = "lynn"
-	fmt.Println(name3)
+	fmt.Println(multiply(2, 2))
 }
