febf0c8f82c534d7f067ccab293091752d922088
daengdaenglee <gunho1020@gmail.com>
2020-03-10 11:44:18 +0900

    array 와 slice

--- main.go
+++ main.go
@@ -1,19 +1,14 @@
 package main
 
-import (
-	"fmt"
-)
-
-func canIDring(age int) bool {
-	switch koreanAge := age + 2; koreanAge {
-	case 10:
-		return false
-	case 18:
-		return true
-	}
-	return false
-}
+import "fmt"
 
 func main() {
-	fmt.Println(canIDring(16))
+	names1 := [5]string{"noci", "lynn", "dal"}
+	names1[3] = "alala"
+	names1[4] = "alala"
+	fmt.Println(names1)
+
+	names2 := []string{"nico", "lynn", "dal"}
+	names2 = append(names2, "flynn")
+	fmt.Println(names2)
 }
