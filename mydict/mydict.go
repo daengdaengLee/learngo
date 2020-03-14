2b86debaa6f6e1ff9d669ca1af58966ffec6bbce
daengdaenglee <gunho1020@gmail.com>
2020-03-15 01:05:24 +0900

    타입 메소드 2

--- mydict/mydict.go
+++ mydict/mydict.go
@@ -1,11 +1,14 @@
 package mydict
 
-import "errors"
+import (
+	"errors"
+)
 
 // Dictionary type
 type Dictionary map[string]string
 
 var errNotFound = errors.New("Not Found")
+var errWordExists = errors.New("That word already exists")
 
 // Search for a word
 func (d Dictionary) Search(word string) (string, error) {
@@ -14,4 +17,16 @@ func (d Dictionary) Search(word string) (string, error) {
 		return value, nil
 	}
 	return "", errNotFound
+}
+
+// Add a word to the dictionary
+func (d Dictionary) Add(word, def string) error {
+	_, err := d.Search(word)
+	switch err {
+	case errNotFound:
+		d[word] = def
+	case nil:
+		return errWordExists
+	}
+	return nil
 }
