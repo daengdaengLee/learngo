0ec1d5c2c6cc2b12d40063fe677b9dab8bbdf7d5
daengdaenglee <gunho1020@gmail.com>
2020-03-15 00:22:21 +0900

    매소드

--- accounts/accounts.go
+++ accounts/accounts.go
@@ -1,13 +1,37 @@
 package accounts
 
+import "errors"
+
 // Account struct
 type Account struct {
 	owner   string
 	balance int
 }
 
+var errNoMoney = errors.New("Can't withdraw you are poor")
+
 // NewAccount create Account
 func NewAccount(owner string) *Account {
 	account := Account{owner: owner, balance: 0}
 	return &account
+}
+
+// Deposite x amount on your account
+func (a *Account) Deposite(amount int) {
+	a.balance += amount
+}
+
+// Balance of your account
+func (a Account) Balance() int {
+	return a.balance
+}
+
+// Withdraw x amount from your account
+func (a *Account) Withdraw(amount int) error {
+	if a.balance < amount {
+		return errNoMoney
+	}
+
+	a.balance -= amount
+	return nil
 }
--- main.go
+++ main.go
@@ -8,5 +8,12 @@ import (
 
 func main() {
 	account := accounts.NewAccount("nico")
-	fmt.Println(account)
+	account.Deposite(10)
+	fmt.Println(account.Balance())
+	err := account.Withdraw(20)
+	if err != nil {
+		// log.Fatalln(err)
+		fmt.Println(err)
+	}
+	fmt.Println(account.Balance())
 }
